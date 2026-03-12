f42caa6250cc70bd9d6e6bf9ccb48c25f7ed1a1b
NeabCAT <neabkub01@gmail.com>
2026-03-12 10:49:02 +0700

    ScriptsMagnus

---
+++ Assets/Scripts/MagnusEffectKick.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MagnusEffectKick : MonoBehaviour
+{
+    public float kickForec;
+    public float spinAmount;
+    public float magnusStrength = 0.5f;
+
+    Rigidbody rb;
+    bool isShot = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isShot)
+        {
+            rb.AddForce(Vector3.forward * kickForec, ForceMode.Impulse);
+
+            rb.AddTorque(Vector3.up * spinAmount);
+
+            isShot = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isShot) return;
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 spin = rb.angularVelocity;
+
+        Vector3 magnusForce = magnusStrength * Vector3.Cross(spin, velocity);
+
+        rb.AddForce(magnusForce);
+    }
+}
